deb17ab5172b6dfc282fb49b1ca1eca7fbc3195b
heyuanlong <1023417614@qq.com>
2015-07-24 15:28:50 +0800

    asdfghj

--- jysock.cpp
+++ jysock.cpp
@@ -36,8 +36,8 @@ int jysock::jyread(int fd,char *buf)
      int nread;
 	 printf("下面是recv\n");
 
-     //nread=recv(fd,buf,1024,0);
-     nread=jyreadn(fd,buf,1024);
+     nread=recv(fd,buf,1024,0);
+    // nread=jyreadn(fd,buf,1024);
 	 printf("recv:%d\n-------revc 完毕\n",nread);
      return nread;
  }
@@ -45,8 +45,8 @@ int jysock::jywrite(int fd,char *buf,size_t len)
 {
     int nread;
 	 printf("下面是send\n");
-    //nread=send(fd,buf,len,0);
-    nread = jywriten(fd,buf,len);
+    nread=send(fd,buf,len,0);
+    //nread = jywriten(fd,buf,len);
 	printf("send:%d\n--------send 完毕\n",nread);
     return nread;
 }
